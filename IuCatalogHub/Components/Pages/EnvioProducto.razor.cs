3f3b887ddcb71517b982fea061d01cc683d7cc99
rarrdev <168307027+cerebroapps@users.noreply.github.com>
2025-12-08 20:21:10 -0500

    Primer commit

--- IuCatalogHub/Components/Pages/EnvioProducto.razor.cs
+++ IuCatalogHub/Components/Pages/EnvioProducto.razor.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using LnCatalogHub.Despachos;
+using Microsoft.AspNetCore.Components;
 using Models;
+using MudBlazor;
 
 namespace IuCatalogHub.Components.Pages
 {
@@ -7,19 +9,44 @@ namespace IuCatalogHub.Components.Pages
     {
 
         [Parameter] public int id { get; set; }
+        private UserModel usuLogueado = new();
+        List<EnvioModel> lsEnvios = new List<EnvioModel>();
+        private EnvioModel envioSelected = null;
+        public bool _loading = true;
+
+
+
+
+
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
             if (firstRender)
             {
-                
+                await CargarEnvios(id);
 
             }
             StateHasChanged();
         }
 
+        private async Task CargarEnvios(int id)
+        {
+            var resp = await _serEnvios.ConsultarEnvios(id);
+            if (!resp.IsError)
+            {
+                lsEnvios = resp.Info;
+                if(lsEnvios.Count > 0)
+                {
+                    envioSelected = lsEnvios.FirstOrDefault();
+                }
+                
 
-
-
-
+            }
+            else
+            {
+                _snackBar.Add(resp.MensajeError, Severity.Error, c => c.SnackbarVariant = Variant.Outlined);
+                return;
+            }
+            _loading = false;
+        }
     }
 }
