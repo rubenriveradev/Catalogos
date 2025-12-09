ececc50efc47ac9ca8b893f3a71be8c06fc3d2c6
rarrdev <168307027+cerebroapps@users.noreply.github.com>
2025-12-08 20:47:10 -0500

    segundo

--- IuCatalogHub/Components/Pages/EnvioProducto.razor.cs
+++ IuCatalogHub/Components/Pages/EnvioProducto.razor.cs
@@ -15,6 +15,10 @@ namespace IuCatalogHub.Components.Pages
         public bool _loading = true;
 
 
+        string _productoBuscar;
+        private Products productSelected = null;
+        private MudTable<Products> tableRef;
+
 
 
 
@@ -23,6 +27,7 @@ namespace IuCatalogHub.Components.Pages
             if (firstRender)
             {
                 await CargarEnvios(id);
+                await CargarProductos();
 
             }
             StateHasChanged();
@@ -48,5 +53,71 @@ namespace IuCatalogHub.Components.Pages
             }
             _loading = false;
         }
+
+        List<Products> lsProductos = new List<Products>();
+
+
+        private async Task<TableData<Products>> LoadServerData(TableState state, CancellationToken cancellationToken)
+        {
+            if (lsProductos.Count == 0)
+            {
+                await CargarProductos();
+            }
+            IEnumerable<Products> data = lsProductos;
+            if (!string.IsNullOrWhiteSpace(_productoBuscar))
+            {
+                data = data.Where(x =>
+                    (x.ProductName?.Contains(_productoBuscar, StringComparison.OrdinalIgnoreCase) ?? false) ||
+                    (x.ItemID?.Contains(_productoBuscar, StringComparison.OrdinalIgnoreCase) ?? false) ||
+                    (x.UPC?.Contains(_productoBuscar, StringComparison.OrdinalIgnoreCase) ?? false));
+            }
+            var pagedData = data
+                .Skip(state.Page * state.PageSize)
+                .Take(state.PageSize)
+                .ToList();
+
+            return await Task.FromResult(new TableData<Products>
+            {
+                TotalItems = data.Count(),
+                Items = pagedData
+            });
+
+
+        }
+        private async Task CargarProductos()
+        {
+            var resp = await _serProductos.ConsultarProductos(0);
+            if (!resp.IsError)
+            {
+                lsProductos = resp.Info;
+            }
+            else
+            {
+                _snackBar.Add(resp.MensajeError, Severity.Error, c => c.SnackbarVariant = Variant.Outlined);
+                return;
+            }
+            _loading = false;
+        }
+
+        private void TriggerReload()
+        {
+            tableRef?.ReloadServerData();
+        }
+
+        private async Task AdicionarProductoEnvio(Products prod)
+        {
+
+        }
+
+
+
+
+
+
+
+
+
+
+
     }
 }
